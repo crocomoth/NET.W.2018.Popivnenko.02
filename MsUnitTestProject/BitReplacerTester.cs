82c60e494a1b5cb9e9fdd01475fed5595ebb931f
crocomoth <justletmeplaymf@gmail.com>
2018-03-15 17:35:38 +0300

    Ms Unit
    ms unit tests added for bitreplacer

---
+++ MsUnitTestProject/BitReplacerTester.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NET.W._2018.Popivnenko._02.BitReplacerProj;
+
+namespace MsUnitTestProject
+{
+    [TestClass]
+    public class BitReplacerTester
+    {
+        BitReplacer bitReplacer = new BitReplacer();
+        [TestMethod]
+        public void BasicTest1()
+        {
+            
+            int someValue = bitReplacer.ReplaceBits(15, 15, 0, 0);
+            Assert.AreEqual(15, someValue);
+        }
+
+        [TestMethod]
+        public void BasicTest2()
+        {
+            int someValue = bitReplacer.ReplaceBits(8, 15, 0, 0);
+            Assert.AreEqual(9, someValue);
+        }
+
+        [TestMethod]
+        public void BasicTest3()
+        {
+            int someValue = bitReplacer.ReplaceBits(8, 15, 8, 3);
+            Assert.AreEqual(120, someValue);
+        }
+
+        [TestMethod]
+        public void BasicTest4()
+        {
+            int someValue = bitReplacer.ReplaceBits(45, 14, 3, 1);
+            Assert.AreEqual(45, someValue);
+        }
+
+        [TestMethod]
+        public void BasicTest5()
+        {
+            int someValue = bitReplacer.ReplaceBits(15, 15, 0, 0);
+            Assert.AreEqual(15, someValue);
+        }
+    }
+}
---
+++ MsUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MsUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MsUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0b3d7436-593b-4e50-99fb-3176c5b6ffe8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
